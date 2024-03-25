c41eb7bcc88a092dc364e84b27f2109160fa02b7
jahnavipullakura <jahnavipullakura>
2024-03-25 14:44:02 +0000

    Pushed from Locofy Builder

---
+++ web/frontend/src/pages/HomeFull.tsx
@@ -0,0 +1,344 @@
+import { FunctionComponent } from "react";
+import styles from "./HomeFull.module.css";
+
+const HomeFull: FunctionComponent = () => {
+  return (
+    <div className={styles.homeFull}>
+      <div className={styles.macbookAir5}>
+        <div className={styles.nutryfoodParent}>
+          <div className={styles.nutryfood}>NUTRYFOOD</div>
+          <div className={styles.crownedWithInnovations}>
+            CROWNED WITH INNOVATIONS
+          </div>
+        </div>
+        <div className={styles.homeParent}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.home}>About Us</div>
+          <div className={styles.home}>Products</div>
+          <div className={styles.home}>Contact Us</div>
+        </div>
+        <div className={styles.component38Parent}>
+          <img
+            className={styles.component38Icon}
+            alt=""
+            src="/component-38.svg"
+          />
+          <img
+            className={styles.component38Icon}
+            alt=""
+            src="/component-39.svg"
+          />
+          <img
+            className={styles.component38Icon}
+            alt=""
+            src="/component-40.svg"
+          />
+          <img
+            className={styles.component38Icon}
+            alt=""
+            src="/component-41.svg"
+          />
+          <img
+            className={styles.component38Icon}
+            alt=""
+            src="/component-42.svg"
+          />
+        </div>
+        <div className={styles.nutryfoodAllRights}>
+          © 2024, NutryFood. All rights reserved.
+        </div>
+      </div>
+      <div className={styles.dbb67e7Fd3848a7Abaf3790588Parent}>
+        <img
+          className={styles.dbb67e7Fd3848a7Abaf3790588Icon}
+          alt=""
+          src="/8dbb67e7fd3848a7abaf379058830e6a-2@2x.png"
+        />
+        <div className={styles.unitingGlobalFlavors}>
+          “Uniting global flavors with authentic simplicity”
+        </div>
+      </div>
+      <div className={styles.frameParent}>
+        <div className={styles.frameGroup}>
+          <div className={styles.crownedWithInnovationsParent}>
+            <div className={styles.crownedWithInnovations1}>
+              Crowned with Innovations
+            </div>
+            <b className={styles.elevatingCulinaryExcellence}>
+              “Elevating Culinary Excellence”
+            </b>
+          </div>
+          <div className={styles.atNutryfoodOurSuccessReWrapper}>
+            <div className={styles.atNutryfoodOur}>
+              At “Nutryfood”, our success revolves around supreme Research and
+              Development, which helps us turn a long, tiring one-day process
+              into a quick one-minute task, making kitchen work much easier.
+              Effortless Cooking starts with us, where convenience and high
+              quality come together in every dish we prepare. We take pride in
+              Redefining Taste Through Research Excellence, ensuring that every
+              bite reflects our dedication to perfection. We are the place Where
+              Research Meets Nourishment, combining innovation and traditional
+              Indian flavors. Our goal is to let people worldwide enjoy
+              authentic Indian cuisine without compromising on Nutrition,
+              Quality, or Taste. Our solutions are also Cost-Effective, making
+              excellence accessible to all. Join us for a journey where culinary
+              creativity meets innovation, promising an amazing dining
+              experience for everyone.
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameContainer}>
+          <div className={styles.groupParent}>
+            <img className={styles.frameChild} alt="" src="/group-206.svg" />
+            <img className={styles.frameChild} alt="" src="/group-207.svg" />
+            <img className={styles.frameChild} alt="" src="/group-208.svg" />
+            <img className={styles.frameChild} alt="" src="/group-209.svg" />
+            <img className={styles.frameChild} alt="" src="/group-210.svg" />
+          </div>
+          <div className={styles.nutritionallySuperiorOptionParent}>
+            <div className={styles.nutritionallySuperiorOption}>
+              Nutritionally superior option
+            </div>
+            <div className={styles.ancientTimeHonoredMethods}>
+              Ancient, time-honored methods
+            </div>
+            <div className={styles.noPreservativesPure}>
+              No preservatives, pure freshness
+            </div>
+            <div className={styles.accessibleWorldwideEverywhe}>
+              Accessible worldwide, everywhere
+            </div>
+            <div className={styles.affordableWithoutSacrificing}>
+              Affordable without sacrificing quality
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.whyNutryfoods}>Why NutryFoods?</div>
+      <div className={styles.component48Parent}>
+        <div className={styles.component48}>
+          <div className={styles.dayProcessTo1MinProcessWrapper}>
+            <div className={styles.dayProcessTo}>
+              1 day process to 1 min process
+            </div>
+          </div>
+          <img
+            className={styles.pendingIcon1}
+            alt=""
+            src="/pendingicon-1.svg"
+          />
+        </div>
+        <div className={styles.component47}>
+          <div className={styles.effortlessCookingStartsWithWrapper}>
+            <div className={styles.effortlessCookingStarts}>
+              Effortless Cooking Starts with Nutryfood
+            </div>
+          </div>
+          <img
+            className={styles.easyToUseIcon1}
+            alt=""
+            src="/easytouseicon-1.svg"
+          />
+        </div>
+        <div className={styles.component49}>
+          <div className={styles.redefiningTasteThroughReseaWrapper}>
+            <div className={styles.redefiningTasteThrough}>
+              Redefining Taste Through Research Excellence
+            </div>
+          </div>
+          <img
+            className={styles.marketResearchIcon1}
+            alt=""
+            src="/marketresearchicon-1.svg"
+          />
+        </div>
+        <div className={styles.component50}>
+          <div className={styles.whereResearchMeetsNourishmeWrapper}>
+            <div className={styles.whereResearchMeets}>
+              Where Research Meets Nourishment
+            </div>
+          </div>
+          <img
+            className={styles.healthyDietIcon1}
+            alt=""
+            src="/healthydieticon-1.svg"
+          />
+        </div>
+        <div className={styles.component51}>
+          <div className={styles.theUltimateCookingShortcutWrapper}>
+            <div className={styles.redefiningTasteThrough}>
+              The ultimate cooking shortcut
+            </div>
+          </div>
+          <img
+            className={styles.exportShareIcon1}
+            alt=""
+            src="/exportshareicon-1.svg"
+          />
+        </div>
+        <div className={styles.component52}>
+          <div className={styles.dishesSpeakOfNutryfoodsRWrapper}>
+            <div
+              className={styles.dishesSpeakOf}
+            >{`Dishes speak of NutryFood's R&D dedication.`}</div>
+          </div>
+          <img
+            className={styles.soupCupAndSoupIcon1}
+            alt=""
+            src="/soupcupandsoupicon-1.svg"
+          />
+        </div>
+        <img className={styles.groupChild} alt="" src="/group-226.svg" />
+        <img className={styles.groupItem} alt="" src="/group-227@2x.png" />
+        <img className={styles.groupItem} alt="" src="/group-228@2x.png" />
+        <div className={styles.ellipseParent}>
+          <div className={styles.ellipseDiv} />
+          <div className={styles.nf}>NF</div>
+        </div>
+      </div>
+      <div className={styles.bestSellersParent}>
+        <div className={styles.home}>Best Sellers</div>
+        <div className={styles.frameDiv}>
+          <div className={styles.frontViewCoffeeBagMockup1Parent}>
+            <img
+              className={styles.frontViewCoffeeBagMockup1Icon}
+              alt=""
+              src="/frontviewcoffeebagmockup-140701391transformed-1@2x.png"
+            />
+            <div className={styles.readyToMix}>Ready to Mix Dosa Powder</div>
+          </div>
+          <div className={styles.frontViewCoffeeBagMockup1Parent}>
+            <img
+              className={styles.frontViewCoffeeBagMockup1Icon}
+              alt=""
+              src="/frontviewcoffeebagmockup-140701391transformed-1@2x.png"
+            />
+            <div className={styles.readyToMix}>Ready to Mix Dosa Powder</div>
+          </div>
+          <div className={styles.frontViewCoffeeBagMockup1Parent}>
+            <img
+              className={styles.frontViewCoffeeBagMockup1Icon}
+              alt=""
+              src="/frontviewcoffeebagmockup-140701391transformed-1@2x.png"
+            />
+            <div className={styles.readyToMix}>Ready to Mix Dosa Powder</div>
+          </div>
+          <div className={styles.frontViewCoffeeBagMockup1Parent}>
+            <img
+              className={styles.frontViewCoffeeBagMockup1Icon}
+              alt=""
+              src="/frontviewcoffeebagmockup-140701391transformed-1@2x.png"
+            />
+            <div className={styles.readyToMix}>Ready to Mix Dosa Powder</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.certificationsParent}>
+        <div className={styles.home}>Certifications</div>
+        <div className={styles.component45}>
+          <img className={styles.image71Icon} alt="" src="/image-71@2x.png" />
+          <img className={styles.image80Icon} alt="" src="/image-71@2x.png" />
+          <img className={styles.image72Icon} alt="" src="/image-72@2x.png" />
+          <img className={styles.image81Icon} alt="" src="/image-72@2x.png" />
+          <img className={styles.image73Icon} alt="" src="/image-73@2x.png" />
+          <img className={styles.image82Icon} alt="" src="/image-73@2x.png" />
+          <img className={styles.image74Icon} alt="" src="/image-74@2x.png" />
+          <img className={styles.image83Icon} alt="" src="/image-74@2x.png" />
+          <img className={styles.image75Icon} alt="" src="/image-75@2x.png" />
+          <img className={styles.image84Icon} alt="" src="/image-75@2x.png" />
+          <img className={styles.image76Icon} alt="" src="/image-76@2x.png" />
+          <img className={styles.image85Icon} alt="" src="/image-76@2x.png" />
+          <img className={styles.image77Icon} alt="" src="/image-77@2x.png" />
+          <img className={styles.image86Icon} alt="" src="/image-77@2x.png" />
+          <img className={styles.image78Icon} alt="" src="/image-78@2x.png" />
+          <img className={styles.image87Icon} alt="" src="/image-78@2x.png" />
+          <img className={styles.image79Icon} alt="" src="/image-79@2x.png" />
+          <img className={styles.image88Icon} alt="" src="/image-79@2x.png" />
+        </div>
+      </div>
+      <div className={styles.whoWeAreParent}>
+        <div className={styles.home}>Who we are?</div>
+        <div className={styles.frameParent1}>
+          <div className={styles.frameChild2} />
+          <div className={styles.welcomeToNutryfoodContainer}>
+            <p className={styles.welcomeToNutryfood}>
+              Welcome to Nutryfood, where culinary innovation meets tradition.
+              With our supreme Research and Development, we've revolutionized
+              cooking, turning hours of effort into minutes of convenience.
+              Effortless Cooking is our mantra, where quality and convenience
+              unite in every dish.
+            </p>
+            <p className={styles.welcomeToNutryfood}>&nbsp;</p>
+            <p className={styles.welcomeToNutryfood}>
+              We redefine taste through Research Excellence, blending innovative
+              techniques with traditional Indian flavors. Our mission is to
+              share authentic Indian cuisine globally, without compromising on
+              Nutrition, Quality, or Taste.
+            </p>
+            <p className={styles.welcomeToNutryfood}></p>
+            <p className={styles.welcomeToNutryfood}>
+              At Nutryfood, excellence is accessible to all, with cost-effective
+              solutions for every kitchen. Join us on a journey where culinary
+              creativity meets innovation, promising unforgettable dining
+              experiences for all.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.macbookAir2Parent}>
+        <div className={styles.macbookAir2}>
+          <div className={styles.shoppingCart1Parent}>
+            <img
+              className={styles.shoppingCart1Icon}
+              alt=""
+              src="/shoppingcart-1.svg"
+            />
+            <img
+              className={styles.shoppingCart1Icon}
+              alt=""
+              src="/heart-2-2.svg"
+            />
+            <img className={styles.user71} alt="" src="/user-7-1.svg" />
+          </div>
+          <img
+            className={styles.macbookAir2Child}
+            alt=""
+            src="/frame-251.svg"
+          />
+          <img
+            className={styles.macbookAir2Item}
+            alt=""
+            src="/group-257@2x.png"
+          />
+        </div>
+        <div className={styles.macbookAir3}>
+          <div className={styles.homeGroup}>
+            <div className={styles.home}>Home</div>
+            <div className={styles.home}>About Us</div>
+            <div className={styles.home}>Products</div>
+            <div className={styles.home}>Contact Us</div>
+          </div>
+          <div className={styles.phone21Parent}>
+            <img
+              className={styles.mdiwhatsappIcon}
+              alt=""
+              src="/phone-2-1.svg"
+            />
+            <img
+              className={styles.mdiwhatsappIcon}
+              alt=""
+              src="/mail-1-1.svg"
+            />
+            <img
+              className={styles.mdiwhatsappIcon}
+              alt=""
+              src="/mdiwhatsapp.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeFull;
